3068d85eb329f2266b74508f09da861a5c0f548e
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-17 19:47:49 +0100

    fix the indentation

--- init.lua
+++ init.lua
@@ -12,45 +12,45 @@ local function tablefind(tab,el)
 	end
 end
 
-local function register_minion(name, description, texture, node_itemstring, radius)
+local function register_minion(name, description, texture, node_itemstring, drop_node_itemstring, radius)
   
-  minions[name] = {}
+	minions[name] = {}
   
   
-  minetest.register_node(minetest.get_current_modname()..":" .. name,
-    {
-      description = description,
-      tiles = {texture}, 
-    },
+	minetest.register_node(minetest.get_current_modname()..":" .. name,
+		{
+			description = description,
+			tiles = {texture}, 
+		},
     
-    on_place = function(itemstack, placer, pointed_thing)
-      table.insert(minions[name], pointed_thing.above)
-    end,
+		on_place = function(itemstack, placer, pointed_thing)
+			table.insert(minions[name], pointed_thing.above)
+		end,
     
-    on_dig = function(pos, node, player)
-      	local tablenumber = tablefind(minions[name], pos)
-	      table.remove(minions[name], tonumber(tablenumber))
-    end,
-    on_construct = function(pos)
-		  minetest.get_meta(pos):set_string("formspec", chest_fs)
-      minetest.get_inventory({type = "node", pos = pos}):set_size("tc", 24)
-	  end,
-  ))
+		on_dig = function(pos, node, player)
+			local tablenumber = tablefind(minions[name], pos)
+			table.remove(minions[name], tonumber(tablenumber))
+		end,
+		
+		on_construct = function(pos)
+			minetest.get_meta(pos):set_string("formspec", chest_fs)
+			minetest.get_inventory({type = "node", pos = pos}):set_size("tc", 24)
+		end,
+	)
   
-
-  local timer = 0
-  minetest.register_globalstep(function(dtime)
-	  timer = timer + dtime;
-	  if timer >= 1 then
-		  for _, minion_pos in minions[name] do
-        local pos1 = vector.add(minion_pos, {x = radius, y = radius, z = radius})
-        local pos2 = vector.add(minion_pos, {x = -radius, y = -radius, z = -radius})
-        for _, pos in minetest.find_nodes_in_area(pos1, pos2, node_itemstring) do
-          minetest.remove_node(pos)
-          minetest.get_inventory({type = "node", pos = pos}):add_stack("tc", node_itemstring)
-        end
-      end
-		  timer = 0
-	  end
-  end)
+	local timer = 0
+	minetest.register_globalstep(function(dtime)
+		timer = timer + dtime;
+		if timer >= 1 then
+			for _, minion_pos in minions[name] do
+				local pos1 = vector.add(minion_pos, {x = radius, y = radius, z = radius})
+				local pos2 = vector.add(minion_pos, {x = -radius, y = -radius, z = -radius})
+				for _, pos in minetest.find_nodes_in_area(pos1, pos2, node_itemstring) do
+					minetest.remove_node(pos)
+					minetest.get_inventory({type = "node", pos = pos}):add_stack("tc", drop_node_itemstring)
+				end
+			end
+		timer = 0
+		end
+	end)
 end
